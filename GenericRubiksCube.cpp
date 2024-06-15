5ff42c709d7b81aef9498529f61e73e8a1cd0920
Gayatri Verma <gayatriverma.ald@gmail.com>
2024-06-15 09:06:44 +0530

    Added GenericRubiksCube Model

---
+++ GenericRubiksCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gayatri on 15-06-2024.
+//
+
+#include "GenericRubiksCube.h"
---
+++ GenericRubiksCube.h
@@ -0,0 +1,16 @@
+//
+// Created by gayatri on 15-06-2024.
+//
+
+#ifndef GENERICRUBIKSCUBE_H
+#define GENERICRUBIKSCUBE_H
+
+
+
+class GenericRubiksCube {
+
+};
+
+
+
+#endif //GENERICRUBIKSCUBE_H
